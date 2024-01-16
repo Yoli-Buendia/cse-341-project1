7280690aa2baf3ed0077bf2bd00fc0fca1c7a872
Yoli-Buendia <bue17003@byui.edu>
2024-01-15 22:10:34 -0600

    initial checkin for project 1

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require ('express');//add library express to this program
+
+//const mongodb = require('./data/database');
+
+const app = express(); //instance to express
+
+const port = process.env.PORT || 3000; //declaring a port with a local variable
+
+app.use('/', require('./routes'));//include routes
+
+app.listen(port, () => (console.log(`Running on port ${port}`)));//listenning for traffic on the port
+
+//mongodb.initDb((err) => {
+   // if(err) {
+        //console.log(err);
+    //}
+    //else{
+        //app.listen(port, () => (console.log(`Database is lsitenning and node running on port ${port}`)));
+    //}
+//});
