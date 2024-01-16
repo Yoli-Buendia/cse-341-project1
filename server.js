696b22ab116ba0a16a25355e203b379862c00390
Yoli-Buendia <bue17003@byui.edu>
2024-01-15 23:28:42 -0600

    connected node project to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ('express');//add library express to this program
 
-//const mongodb = require('./data/database');
+const mongodb = require('./data/database');//add mongo library
 
 const app = express(); //instance to express
 
@@ -8,13 +8,13 @@ const port = process.env.PORT || 3000; //declaring a port with a local variable
 
 app.use('/', require('./routes'));//include routes
 
-app.listen(port, () => (console.log(`Running on port ${port}`)));//listenning for traffic on the port
+//app.listen(port, () => (console.log(`Running on port ${port}`)));//listenning for traffic on the port
 
-//mongodb.initDb((err) => {
-   // if(err) {
-        //console.log(err);
-    //}
-    //else{
-        //app.listen(port, () => (console.log(`Database is lsitenning and node running on port ${port}`)));
-    //}
-//});
+mongodb.initDb((err) => {
+   if(err) {
+        console.log(err);
+    }
+    else{
+        app.listen(port, () => (console.log(`Database is lsitenning and node running on port ${port}`)));
+    }
+});
