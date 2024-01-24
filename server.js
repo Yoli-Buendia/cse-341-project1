2f6c1c363d49a8ef54149e9d413bc79efb58e2ff
Yoli-Buendia <bue17003@byui.edu>
2024-01-23 22:39:26 -0600

    post, get, put, delete

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require ('express');//add library express to this program
-
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');//add mongo library
-
 const app = express(); //instance to express
 
 const port = process.env.PORT || 3000; //declaring a port with a local variable
 
+app.use(bodyParser.json());
 app.use('/', require('./routes'));//include routes
 
-//app.listen(port, () => (console.log(`Running on port ${port}`)));//listenning for traffic on the port
+
 
 mongodb.initDb((err) => {
    if(err) {
